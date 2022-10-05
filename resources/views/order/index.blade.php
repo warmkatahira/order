bc9817fd0dfbbeba81f5e93d1c37eb9add247544
warmkatahira <t.katahira@warm.co.jp>
2022-10-05 14:07:27 +0900

    commit_20221005

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -30,37 +30,39 @@
             @component('components.item-search')
             @endcomponent
         </form>
-        <table class="col-span-12">
-            <thead>
-                <tr class="text-xs xl:text-sm text-left text-white bg-gray-600 border-gray-600">
-                    <th class="font-thin p-2 px-2 w-2/12">商品コード</th>
-                    <th class="font-thin p-2 px-2 w-2/12">商品カテゴリ</th>
-                    <th class="font-thin p-2 px-2 w-1/12">商品JANコード</th>
-                    <th class="font-thin p-2 px-2 w-5/12">商品名</th>
-                    <th class="font-thin p-2 px-2 w-1/12 text-right">有効在庫数</th>
-                    <th class="font-thin p-2 px-2 w-1/12 text-center">発注</th>
-                </tr>
-            </thead>
-            <tbody class="bg-white">
-                @foreach($items as $item)
-                    <tr class="text-xs xl:text-sm h-14 hover:bg-teal-100">
-                        <td class="p-1 px-2 border">{{ $item->item_code }}</td>
-                        <td class="p-1 px-2 border">{{ $item->item_category }}</td>
-                        <td class="p-1 px-2 border">{{ $item->item_jan_code }}</td>
-                        <td class="p-1 px-2 border">{{ $item->item_name }}</td>
-                        <td class="p-1 px-2 border text-right">{{ number_format(GetAvailableStockQuantityFunc::GetAvailableStockQuantityFunc($item->item_id)) }}</td>
-                        <td class="p-1 px-2 border text-center">
-                            <!-- 有効在庫数が1以上あれば発注ボタンを表示、なければボタンを非表示にする -->
-                            @if(GetAvailableStockQuantityFunc::GetAvailableStockQuantityFunc($item->item_id) > 0)
-                                <button type="button" id="{{ $item->item_id }}" class="order_in text-center"><i class="las la-cart-plus la-2x"></i></button>
-                            @else
-                                <p class="text-sm">在庫なし</p>
-                            @endif
-                        </td>
+        <div class="col-span-12 grid grid-cols-12 overflow-x-auto">
+            <table class="col-span-12 min-w-full">
+                <thead>
+                    <tr class="text-xs xl:text-sm text-left text-white bg-gray-600 border-gray-600 whitespace-nowrap">
+                        <th class="font-thin p-2 px-2 w-2/12">商品コード</th>
+                        <th class="font-thin p-2 px-2 w-2/12">商品カテゴリ</th>
+                        <th class="font-thin p-2 px-2 w-1/12">商品JANコード</th>
+                        <th class="font-thin p-2 px-2 w-5/12">商品名</th>
+                        <th class="font-thin p-2 px-2 w-1/12 text-right">有効在庫数</th>
+                        <th class="font-thin p-2 px-2 w-1/12 text-center">発注</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="bg-white">
+                    @foreach($items as $item)
+                        <tr class="text-xs xl:text-sm h-14 hover:bg-teal-100 whitespace-nowrap">
+                            <td class="p-1 px-2 border">{{ $item->item_code }}</td>
+                            <td class="p-1 px-2 border">{{ $item->item_category }}</td>
+                            <td class="p-1 px-2 border">{{ $item->item_jan_code }}</td>
+                            <td class="p-1 px-2 border">{{ $item->item_name }}</td>
+                            <td class="p-1 px-2 border text-right">{{ number_format(GetAvailableStockQuantityFunc::GetAvailableStockQuantityFunc($item->item_id)) }}</td>
+                            <td class="p-1 px-2 border text-center">
+                                <!-- 有効在庫数が1以上あれば発注ボタンを表示、なければボタンを非表示にする -->
+                                @if(GetAvailableStockQuantityFunc::GetAvailableStockQuantityFunc($item->item_id) > 0)
+                                    <button type="button" id="{{ $item->item_id }}" class="order_in text-center"><i class="las la-cart-plus la-2x"></i></button>
+                                @else
+                                    <p class="text-sm">在庫なし</p>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         <!-- ページネーション -->
         <div class="col-span-12 mt-3">
             {{ $items->appends(request()->input())->links() }}
